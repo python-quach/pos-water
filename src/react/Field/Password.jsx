433065dd30e373c6239f812a0c2ca593fddb128c
Hung Quach <hung.quach@gmail.com>
2021-01-31 11:29:00 -0800

    refractor password

--- src/react/Field/Password.jsx
+++ src/react/Field/Password.jsx
@@ -1,25 +1,26 @@
 import { Field } from 'react-final-form';
 import { Form } from 'semantic-ui-react';
 
-const Password = (props) => {
-    const { iconColor } = props;
-    return (
-        <Field
-            id='password'
-            name='password'
-            placeholder='password'
-            component={Form.Input}
-            className={iconColor}
-            type='password'
-            size='massive'
-            icon='lock'
-            iconPosition='left'
-            focus
-            fluid
-            transparent
-            inverted
-        />
-    );
-};
+const Password = ({ iconColor }) => (
+    <Field
+        name='password'
+        render={({ input, meta }) => (
+            <Form.Input
+                {...input}
+                id='password'
+                placeholder='password'
+                className={iconColor}
+                type='password'
+                size='massive'
+                icon='lock'
+                iconPosition='left'
+                focus
+                fluid
+                transparent
+                inverted
+            />
+        )}
+    />
+);
 
 export default Password;
--- src/react/Field/index.js
+++
@@ -1,7 +0,0 @@
-import Password from './Password';
-import Username from './Username';
-
-export const Field = {
-    Password,
-    Username,
-};

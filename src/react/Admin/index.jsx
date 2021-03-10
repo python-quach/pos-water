19d4d7de53b33f26d3ca69015701608e0d31781c
Hung Quach <hung.quach@gmail.com>
2021-03-09 18:47:18 -0800

    remove route

--- src/react/Admin/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Header, Button } from 'semantic-ui-react';
-
-const Admin = ({ history }) => {
-    return <Button onClick={() => history.push('/')}>Admin Page</Button>;
-};
-
-export default Admin;

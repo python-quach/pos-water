4876e034b277ebea3bda5bb0276544a386a3a835
Hung Quach <hung.quach@gmail.com>
2021-03-21 15:56:44 -0700

    rempove menu

--- electron/electron.js
+++ electron/electron.js
@@ -126,7 +126,7 @@ function createWindow() {
         },
     });
 
-    // mainWindow.removeMenu();
+    mainWindow.removeMenu();
     mainWindow.loadURL(startUrl);
     mainWindow.on('closed', function () {
         mainWindow = null;
